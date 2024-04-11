26c843f249efb6ec9cfaba2f156ecb7e558f7e0d
carzgil <cars.gilliland@gmail.com>
2024-04-11 21:51:04 +0000

    customer_steps before bug fix

---
+++ features/steps/customer_steps.py
@@ -0,0 +1,59 @@
+######################################################################
+# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
+"""
+Customer Steps
+
+Steps file for customer.feature
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+import requests
+from behave import given
+
+# HTTP Return Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+
+@given('the following customers')
+def step_impl(context):
+    """ Delete all Customers and load new ones """
+
+    # List all of the pets and delete them one by one
+    rest_endpoint = f"{context.base_url}/customers"
+    context.resp = requests.get(rest_endpoint)
+    assert(context.resp.status_code == HTTP_200_OK)
+    for customer in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{customer['id']}")
+        assert(context.resp.status_code == HTTP_204_NO_CONTENT)
+
+    # load the database with new pets
+    for row in context.table:
+        payload = {
+            "id": row['id'],
+            "username": row['username'],
+            "password": row['password'],
+            "first_name": row['first_name'],
+            "last_name": row['last_name'],
+            "gender": row['gender'],
+            "active": row['active'] in ['True', 'true', '1'],
+            "address": row['address'],
+            "email": row['email']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert(context.resp.status_code == HTTP_201_CREATED)
